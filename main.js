f445f507d88bf0bd988b5fad3556b0f89fefd4d3
Rodolfo <rudolfrce@gmail.com>
2022-01-28 18:42:33 -0700

    Add random colors and enable reset button

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-// Functions
+// Initial state
 const canvas = document.querySelector("#canvas");
-const canvasElementCreator = (n = 64) => {
-  for (let i = 0, len = n; i < len; i++) {
+
+const pixels = 550;
+const canvasSize = `${pixels}px`;
+canvas.style.minHeight = canvasSize;
+canvas.style.maxHeight = canvasSize;
+canvas.style.minWidth = canvasSize;
+canvas.style.maxWidth = canvasSize;
+
+const canvasElementCreator = (n) => {
+  const numberOfElements = n ** 2;
+  for (let i = 0, len = numberOfElements; i < len; i++) {
     eval(`canvasElement${i} = document.createElement("div")`);
     eval(`canvasElement${i}.classList.add("canvas-element", "element-${i}")`);
     eval(`canvas.appendChild(canvasElement${i})`);
@@ -9,14 +18,27 @@ const canvasElementCreator = (n = 64) => {
 };
 
 // DOM manupulation
-canvasElementCreator();
+numberOfSquaresPerSide = 16;
+canvasElementCreator(numberOfSquaresPerSide);
 
 const hoverRandomColors = (e) => {
-  e.target.style.backgroundColor = `black`;
+  const randomColor = Math.floor(16 ** 6 * Math.random()).toString(16);
+  e.target.style.backgroundColor = `#${randomColor}`;
 };
 
-const canvasElement = document.querySelectorAll("div.canvas-element");
+const canvasElements = document.querySelectorAll("div.canvas-element");
 
-canvasElement.forEach((element) => {
+canvasElements.forEach((element) => {
+  element.style.minHeight = `${pixels / numberOfSquaresPerSide}px`;
+  element.style.maxHeight = `${pixels / numberOfSquaresPerSide}px`;
+  element.style.minWidth = `${pixels / numberOfSquaresPerSide}px`;
+  element.style.maxWidth = `${pixels / numberOfSquaresPerSide}px`;
   element.addEventListener("mouseover", hoverRandomColors);
+});
+
+const resetButton = document.querySelector(".reset");
+resetButton.addEventListener("click", () => {
+  canvasElements.forEach((element) => {
+    element.style.backgroundColor = "white";
+  });
 });
