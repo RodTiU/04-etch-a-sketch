46f1720086430945887be77f644141584cd396b0
Rodolfo <rudolfrce@gmail.com>
2022-01-31 19:44:44 -0700

    Add slider integration

--- main.js
+++ main.js
@@ -8,37 +8,56 @@ canvas.style.maxHeight = canvasSize;
 canvas.style.minWidth = canvasSize;
 canvas.style.maxWidth = canvasSize;
 
-const canvasElementCreator = (n) => {
-  const numberOfElements = n ** 2;
-  for (let i = 0, len = numberOfElements; i < len; i++) {
-    eval(`canvasElement${i} = document.createElement("div")`);
-    eval(`canvasElement${i}.classList.add("canvas-element", "element-${i}")`);
-    eval(`canvas.appendChild(canvasElement${i})`);
-  }
-};
-
-// DOM manupulation
-numberOfSquaresPerSide = 16;
-canvasElementCreator(numberOfSquaresPerSide);
-
 const hoverRandomColors = (e) => {
   const randomColor = Math.floor(16 ** 6 * Math.random()).toString(16);
   e.target.style.backgroundColor = `#${randomColor}`;
 };
 
-const canvasElements = document.querySelectorAll("div.canvas-element");
+const canvasElementCreator = (e) => {
+  const numberOfElements = Number(e) ** 2;
+  for (let i = 0, len = numberOfElements; i < len; i++) {
+    eval(`canvasElement${i} = document.createElement("div")`);
+    eval(`canvasElement${i}.classList.add("canvas-element")`);
+    eval(`canvas.appendChild(canvasElement${i})`);
+  }
+};
 
-canvasElements.forEach((element) => {
+const resetButton = document.querySelector(".clear");
+resetButton.addEventListener("click", () => {
+  const elements = document.querySelectorAll("div.canvas-element");
+  elements.forEach((element) => {
+    element.style.backgroundColor = "white";
+  });
+});
+
+const slideButton = document.querySelector("input");
+slideButton.addEventListener("change", (e) => {
+  // initialize
+  const elementsBefore = document.querySelectorAll("div.canvas-element");
+  elementsBefore.forEach((element) => {
+    element.parentNode.removeChild(element);
+  });
+  // refresh values to input specific range
+  canvasElementCreator(e.target.value);
+  const elementsAfter = document.querySelectorAll("div.canvas-element");
+  const numberOfSquaresPerSide = Number(e.target.value);
+  elementsAfter.forEach((element) => {
+    element.style.minHeight = `${pixels / numberOfSquaresPerSide}px`;
+    element.style.maxHeight = `${pixels / numberOfSquaresPerSide}px`;
+    element.style.minWidth = `${pixels / numberOfSquaresPerSide}px`;
+    element.style.maxWidth = `${pixels / numberOfSquaresPerSide}px`;
+    element.addEventListener("mouseover", hoverRandomColors);
+  });
+});
+
+const initialElements = 10;
+canvasElementCreator(initialElements);
+const numberOfSquaresPerSide = Number(initialElements);
+const elements = document.querySelectorAll("div.canvas-element");
+elements.forEach((element) => {
   element.style.minHeight = `${pixels / numberOfSquaresPerSide}px`;
   element.style.maxHeight = `${pixels / numberOfSquaresPerSide}px`;
   element.style.minWidth = `${pixels / numberOfSquaresPerSide}px`;
   element.style.maxWidth = `${pixels / numberOfSquaresPerSide}px`;
   element.addEventListener("mouseover", hoverRandomColors);
-});
-
-const resetButton = document.querySelector(".reset");
-resetButton.addEventListener("click", () => {
-  canvasElements.forEach((element) => {
-    element.style.backgroundColor = "white";
-  });
 });
